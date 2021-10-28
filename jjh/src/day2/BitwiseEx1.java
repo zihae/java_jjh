c8ab8e891f27663c23c23ec1d3b17977d6a12d09
Jihye Jang <zihae5442@gmail.com>
2021-10-28 16:28:02 +0900

    2-5 : 비트 연산자 예제

---
+++ jjh/src/day2/BitwiseEx1.java
@@ -0,0 +1,27 @@
+package day2;
+
+public class BitwiseEx1 {
+
+	public static void main(String[] args) {
+		int num1 = 3, num2 = 10; 
+		//3  : 00000000 00000000 000000000 00000011
+		//10 : 00000000 00000000 000000000 00001010
+		//and: 00000000 00000000 000000000 00001010 :2
+		//or : 00000000 00000000 000000000 00001011 : 11
+		//xor: 00000000 00000000 000000000 00001011 : 9
+		//not: 11111111 11111111 111111111 11111100 : -4
+		
+		int and = num1 & num2;
+		int or = num1 | num2;
+		int not = ~num1;
+		int xor = num1 ^ num2;
+		
+		System.out.println(num1 + " & " + num2 + " = " + and);
+		System.out.println(num1 + " | " + num2 + " = " + or);
+		System.out.println(num1 + " ^ " + num2 + " = " + xor);
+		System.out.println("~" + num1 + " = " + not);
+		
+
+	}
+
+}
