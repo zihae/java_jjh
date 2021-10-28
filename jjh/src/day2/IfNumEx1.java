d6a82985c065d959503cc4c8d744aa5ad36a8e1d
Jihye Jang <zihae5442@gmail.com>
2021-10-28 17:49:40 +0900

    Update IfNumEx1.java

--- jjh/src/day2/IfNumEx1.java
+++ jjh/src/day2/IfNumEx1.java
@@ -3,8 +3,22 @@ package day2;
 public class IfNumEx1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/* (num가 0보다 크)면 [양수라고 출력]하고
+		 * (num가 0이)면 [0이라고 출력]하고
+		 * (num가 0보다 작으)면 [음수라고 출력]하는 코드를 작성하세요.
+		 * */
+		
+		int num = 0;
+		if(num >0) {
+			System.out.println(num + "는 양수");
+		}
+		else if(num == 0) {
+			System.out.println(num);
+		}else {
+			System.out.println(num + "는 음수"); 
+		}
+		//정수가 0보다 크지 않고, 정수가 0보다 같지 않은 수 = 음수
 	}
+	
 
 }
