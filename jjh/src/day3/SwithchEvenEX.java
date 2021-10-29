1721783be369967a8ab9ea8658f6782948d2dc72
Jihye Jang <zihae5442@gmail.com>
2021-10-29 16:52:02 +0900

    3-5 : switch문 홀짝 예제

---
+++ jjh/src/day3/SwithchEvenEX.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class SwithchEvenEX {
+
+	public static void main(String[] args) {
+		/* switch문을 이용하여 정수의 홀짝 여부를 판별하는 코드를 작성하세요.
+		 * */
+		int num = 10;
+		
+		switch(num % 2) {
+		case 0: 
+			System.out.println(num + "는 짝수");
+			break;
+		default:
+			System.out.println(num + "는 홀수"); 
+	}
+
+}
+}
