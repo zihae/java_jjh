48e6f21da9560d5925db9c2b65952efdb305278c
zihae <zihae5442@gmail.com>
2021-11-02 16:25:27 +0900

    5-9: 이중 반복문 100이하 소수 출력 예제

---
+++ jjh/src/day5/MultipleTableEx1.java
@@ -0,0 +1,28 @@
+package day5;
+
+public class MultipleTableEx1 {
+
+	public static void main(String[] args) {
+		/* 2단에서 9단까지 구구단을 출력하는 코드를 작성하세요.
+		 * 7단 코드를 가져와서 생각
+		 
+		
+		 */
+		
+		int num=2, j;
+		//단을 2에서 9까지 1씩 증가
+		while(num<= 9) {
+			//num단 출력하는 코드
+			j= 1;
+			while(j<=9) {
+				System.out.println(num + " x " + j + " = " + num * j);
+				j++;
+			
+		}
+		num++;
+			
+		}
+
+	}
+
+}
