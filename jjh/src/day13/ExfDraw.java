c713c624344c76526136c584a7b01e3414208291
zihae <zihae5442@gmail.com>
2021-11-12 14:08:01 +0900

    13-5: 인터페이스 도형 예제

--- jjh/src/day13/ExfDraw.java
+++ jjh/src/day13/ExfDraw.java
@@ -1,12 +1,14 @@
 package day13;
-
+/* 도형 크기 변경, 도형 이동, 도형그리기 기능을 가진 ExfDraw인터페이스를 만드세요. */
 public interface ExfDraw {
 	public void resize(int width, int height);
+	
 	public void move(int x, int y);
+	
 	public void draw();
 }
 
-/* ExfDraw 인터페이스를 구현한 ExfShape 클래스를 만드세요. */
+
 
 
 
