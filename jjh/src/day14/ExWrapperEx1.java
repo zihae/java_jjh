6d9d5e45be560458526f7b1e352babb0bb95167b
Jihye Jang <zihae5442@gmail.com>
2021-11-15 13:06:48 +0900

    14-5: Wrapper 클래스 예제

---
+++ jjh/src/day14/ExWrapperEx1.java
@@ -0,0 +1,13 @@
+package day14;
+
+public class ExWrapperEx1 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		print(num);
+
+	}
+	public static void print(Object obj) {
+		System.out.println(obj.toString());
+	}
+}
