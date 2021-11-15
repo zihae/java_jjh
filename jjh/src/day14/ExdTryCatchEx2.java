09fcfeca94270c4bd8a03694a35a381bf2c7f6e3
Jihye Jang <zihae5442@gmail.com>
2021-11-15 17:17:12 +0900

    14-8: 예외 처리 try catch 예제

---
+++ jjh/src/day14/ExdTryCatchEx2.java
@@ -0,0 +1,48 @@
+package day14;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExdTryCatchEx2 {
+
+	public static void main(String[] args) {
+		/* 두 정수와 산술 연산자를 입력받아 입력받은 산술 연산자에 맞는 결과를 출력하는 코드를 작성하세요
+		 * 단, 예외처리를 이용 */
+		Scanner scan = new Scanner(System.in);
+		int num1,num2;
+		char operator;
+		double res =0.0;
+		
+		System.out.print("두 정수와 산술 연산자를 입력하세요 (예 : 1 + 2) : ");
+		try {
+			num1 = scan.nextInt();
+			operator = scan.next().charAt(0);
+			num2 = scan.nextInt();
+			
+			switch(operator) {
+			case '+': res = num1 + num2;
+				break;
+			case '-': res = num1 - num2;
+				break;
+			case '*': res = num1 * num2;
+				break;
+			case '/': res = num1 / num2;
+				break;
+			case '%': res = num1 % num2;
+				break;
+			}
+			System.out.println("" + num1 + operator + num2 + "=" + res);
+		}catch(ArithmeticException e) {
+			System.out.println("0으로 나눌 수 없다");
+		}catch(InputMismatchException e) {
+			System.out.println("입력을 잘못했다");
+		}
+		catch (Exception e) {
+			System.out.println("예외 발생");
+		}
+		scan.close();
+					
+
+	}
+
+}
