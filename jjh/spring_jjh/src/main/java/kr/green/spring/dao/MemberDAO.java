2c6397679b54db078e2f5ea8b33b7fb9b690c677
Jihye Jang <zihae5442@gmail.com>
2022-02-04 17:43:56 +0900

    스프링: 아이디 찾기 적용

--- jjh/spring_jjh/src/main/java/kr/green/spring/dao/MemberDAO.java
+++ jjh/spring_jjh/src/main/java/kr/green/spring/dao/MemberDAO.java
@@ -9,7 +9,10 @@ public interface MemberDAO {
 	MemberVO getMember(@Param("me_id")String me_id);
 	//Select를 제외한 return은 return type이 void
 	void insertMember(@Param("user")MemberVO user);
+	
 	void updateMember(@Param("user")MemberVO input);
+	
+	MemberVO findMember(@Param("user") MemberVO member);
 	
 	
 
